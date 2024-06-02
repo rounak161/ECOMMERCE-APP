4caf01460d0c83561fb0ddfe3016074d3286fb1d
rounak161 <142321950+rounak161@users.noreply.github.com>
2024-06-02 16:04:20 +0530

    Initial Commit

---
+++ client/src/pages/Policy.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Layout from "./../components/Layout/Layout";
+
+const Policy = () => {
+  return (
+    <Layout title={"Privacy Policy"}>
+      <div className="row contactus ">
+        <div className="col-md-6 ">
+          <img
+            src="/images/contactus.jpeg"
+            alt="contactus"
+            style={{ width: "100%" }}
+          />
+        </div>
+        <div className="col-md-4">
+        <h2>Privacy Policy</h2>
+          <p>Welcome to ECOMMERCE APP. Your privacy is critically important to us.</p>
+          <p>Security: The security of your Personal Information is important to us, but remember that no method of transmission over the Internet, or method of electronic storage is 100% secure. While we strive to use commercially acceptable means to protect your Personal Information, we cannot guarantee its absolute security.</p>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default Policy;
